ced3b22457114fbfaa0b84e9be85efafe0e8f233
ronrest <ronny.coding@gmail.com>
2016-05-17 19:24:39 +1000

    MODULE - Add Module Model to admin section

--- teecha/admin.py
+++ teecha/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Lesson
+from .models import Lesson, Module, ModuleLesson
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', "hidden_description")
@@ -15,5 +15,19 @@ class LessonAdmin(admin.ModelAdmin):
               )
 
 
+class ModuleLessonInline(admin.TabularInline):
+    """
+    Allows the ModuleLesson "Through" Model to appear in the admin
+    for Module
+    """
+    model = ModuleLesson
+    extra = 1
+
+class ModuleAdmin(admin.ModelAdmin):
+    # Allows you to add lessons one at a time
+    inlines = (ModuleLessonInline,)
+
 # Register your models here.
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Module, ModuleAdmin)
+
