bd1c5adae42efe708d2e0b0468012ca96e7d9b5b
ronrest <ronny.coding@gmail.com>
2016-05-17 19:27:57 +1000

    TODO - Add a todo to Lesson

--- teecha/models.py
+++ teecha/models.py
@@ -11,6 +11,9 @@ class Lesson(models.Model):
     #
     # TODO: restrict lesson name to not contain any spaces, commas,
     #       forward slashes,colons, semi colons or other chars that
+    # TODO: Look into using slug field for accomplishing the above TODO. But
+    #       see if hyphens can be replced with underscores instead. Also try
+    #       to find a way to ensure uniqueness.
     #       might not filename friendly
     name = models.CharField(max_length=80, blank=False)
 
