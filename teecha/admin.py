f44e403611a65b03820f9add5ce020fbace10447
ronrest <ronny.coding@gmail.com>
2016-05-17 15:32:44 +1000

    ADMIN/LESSON - customise the admin for Lesson model

--- teecha/admin.py
+++ teecha/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Lesson
 
-# Register your models here.
-admin.site.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', "hidden_description")
+
+    fields = ('name',
+              'title',
+              'published',
+              'video',
+              'video_service',
+              'content',
+              'description',
+              'hidden_description',
+              )
 
 
 
