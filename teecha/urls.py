ac61d6a8b1323fea5af7d9eff980eebf48d2dda3
ronrest <ronny.coding@gmail.com>
2016-05-17 15:45:44 +1000

    VIEWS/LESSON - Add view, template, urlconf that handles a single lesson

--- teecha/urls.py
+++ teecha/urls.py
@@ -3,5 +3,6 @@ from . import views                 # import the blog views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^lesson/(?P<id>\d+)$', views.lesson, name="lesson"),
 ]
 
--- teecha/views.py
+++ teecha/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render, HttpResponse
+from django import template
+import markdown
+from .models import Lesson
+
+register = template.Library()
+
+
+# ==============================================================================
+#                                                                    MARKDOWNIFY
+# ==============================================================================
+@register.filter
+def markdownify(text):
+    # safe_mode governs how the function handles raw HTML
+    return markdown.markdown(text, safe_mode='escape')
+
+
+# ==============================================================================
+#                                                                FIX LESSON URLS
+# ==============================================================================
+def fix_lesson_urls(content, lesson_name):
+    return content.replace("LESSON_IMG_DIR", lesson_name)
+
+
 # ==============================================================================
 #                                                                          INDEX
 # ==============================================================================
@@ -6,4 +29,24 @@ def index(request):
     return HttpResponse("This is the index for teecha")
 
 
-# Create your views here.
+# ==============================================================================
+#                                                                         LESSON
+# ==============================================================================
+def lesson(request, id):
+    # TODO: perform error checking to make sure lesson_id is a valid lesson id.
+    #       eg, it must be an integer.
+    lesson = Lesson.objects.get(id=id)
+
+    content = markdownify(lesson.content)
+    content = fix_lesson_urls(content, "/static/teecha/img")
+    context = {
+        "content": content,
+        "title": lesson.title,
+        "video": lesson.video,
+    }
+    # content = lesson.content
+    # title = lesson.title
+    # video = lesson.video
+
+    return render(request, template_name="lesson.html", context=context)
+
