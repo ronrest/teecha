9026774139aaafbe0ce080b923931f1895e73939
ronrest <ronny.coding@gmail.com>
2016-05-22 21:29:53 +1000

    TODO/MODULE - Add TODO for responsive images

--- teecha/views.py
+++ teecha/views.py
@@ -83,7 +83,7 @@ def module_lesson(request, module, lesson):
     lesson = module.lessons.get(name=lesson)
     lesson_num = lesson.modulelesson_set.get(module=module).order # assuming 1 indexing
 
-
+    # TODO: enforce responsive images.
 
     content = markdownify(lesson.content)
     content = fix_lesson_urls(content, "/static/teecha/img")
